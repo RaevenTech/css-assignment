3421593a0d6c514e76fbc3219494016fe526d2bc
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2023-02-27 20:14:51 +0000

    assignment complete

--- assignment-four/src/components/Hero.js
+++ assignment-four/src/components/Hero.js
@@ -7,7 +7,15 @@ const Hero = () => {
     <>
       <div className={styles.body}>
         <Header />
-        <h1 className={styles.h1}>Hero</h1>
+        <div className={styles.main_container}>
+          <img className={styles.hero_img} src="https://images.squarespace-cdn.com/content/v1/5a327bb28fd4d2bd424f9c57/1514900889214-22ZWUTWU90BA5PXPSSF2/upload.jpg" alt="African Sunset" />
+          <h1>This website is awesome</h1>
+          <p>
+            This website has some subtext that goes here under the main title. 
+            It is a smaller font and the color is lower
+          </p>
+          <button> Sighn up </button>
+        </div>
       </div>
     </>
   )
