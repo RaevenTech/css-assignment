d8d4eaf788f57ae4b14f7668937e459e4b628d88
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2023-02-28 16:07:50 +0000

    assignment complete

--- assignment-four/src/components/CallToAction.js
+++ assignment-four/src/components/CallToAction.js
@@ -3,7 +3,17 @@ import styles from "./callToAction.module.css"
 
 const CallToAction = () => {
   return (
-    <h1 className={styles.h1}>CallToAction</h1>
+    <div className={styles.action_body}>
+      <div className={styles.action_container}>
+        <div className={styles.action_txt}>
+          <h3 className={styles.action_title}> Call to action! It's time! </h3>
+          <p className={styles.action_subtxt}>
+            Sign up for our products by clicking the button right over there!
+          </p>
+        </div>
+      <button className={styles.action_btn}> Sign up </button>
+      </div>
+    </div>
   )
 }
 
--- assignment-four/src/components/Quote.js
+++ assignment-four/src/components/Quote.js
@@ -3,7 +3,14 @@ import styles from "./quote.module.css"
 
 const Quote = () => {
   return (
-    <h1 className={styles.h1}>Quote</h1>
+    <div className={styles.quote_body}>
+      <p className={styles.quote_txt}><i>
+        This is an inspiring qoute, or testimonial from a<br/>
+        customer.Maybe it's just filling up space, or maybe<br/>
+        people will actully read it. Who knows? all i know <br/>
+        is that it looks nice.<span className={styles.quote_quoter}><b> - Thor </b></span>
+        </i></p>
+    </div>
   )
 }
 
