8ad0565c9af6fc96e7271a1bfac4eeae55a333de
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2023-02-28 11:15:09 +0000

    assignment complete

--- assignment-four/src/components/Hero.js
+++ assignment-four/src/components/Hero.js
@@ -9,12 +9,13 @@ const Hero = () => {
         <Header />
         <div className={styles.main_container}>
           <img className={styles.hero_img} src="https://images.squarespace-cdn.com/content/v1/5a327bb28fd4d2bd424f9c57/1514900889214-22ZWUTWU90BA5PXPSSF2/upload.jpg" alt="African Sunset" />
-          <h1>This website is awesome</h1>
-          <p>
-            This website has some subtext that goes here under the main title. 
-            It is a smaller font and the color is lower
+          <h1 className={styles.hero_title}>This website is awesome</h1>
+          <p className={styles.hero_txt}>
+            This website has some subtext that goes here under the <br/> 
+            main title. It is a smaller font and the color is lower <br/>
+            contrast.
           </p>
-          <button> Sighn up </button>
+          <button className={styles.hero_btn}> Sign up </button>
         </div>
       </div>
     </>
--- assignment-four/src/components/InfoSection.js
+++ assignment-four/src/components/InfoSection.js
@@ -3,7 +3,28 @@ import styles from "./infoSection.module.css"
 
 const InfoSection = () => {
   return (
-    <h1 className={styles.h1}>InfoSection</h1>
+    <div className={styles.info_body}>
+      <h1 className={styles.info_h1}>Some random infomation</h1>
+      <div className={styles.info_items}>
+        <div className={styles.info_item}>
+          <img  class={styles.info_img} src="" alt="" />
+          <p className={styles.info_txt}> This is some subtext under an illustration or image</p>
+        </div>
+        <div className={styles.info_item}>
+          <img  class={styles.info_img} src="" alt="" />
+          <p className={styles.info_txt}> This is some subtext under an illustration or image</p>
+        </div>
+        <div className={styles.info_item}>
+          <img  class={styles.info_img} src="" alt="" />
+          <p className={styles.info_txt}> This is some subtext under an illustration or image</p>
+        </div>
+        <div className={styles.info_item}>
+          <img  class={styles.info_img} src="" alt="" />
+          <p className={styles.info_txt}> This is some subtext under an illustration or image</p>
+        </div>
+      </div>
+    </div>
+    
   )
 }
 
